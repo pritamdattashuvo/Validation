ae42ecf0774dc41bd4eea94de90dc7369629289e
markza1998 <2411kjn@gmail.com>
2021-09-12 06:45:39 +0700

    add thai regular expression and increase address length

--- Validation/src/Emailvalidation.java
+++ Validation/src/Emailvalidation.java
@@ -2,9 +2,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Emailvalidation {
-	public static String valEmail(){
-		String input="pritamewu@naztech.io";
-		String emailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+	public static String valEmail(String input){
+		String emailRegex = "^[\\u0E00-\\u0E7FA-Z0-9._%+-]+@[\\u0E00-\\u0E7FA-Z0-9.-]+\\.[\\u0E00-\\u0E7FA-Z]{2,20}$";
 		Pattern emailPat = Pattern.compile(emailRegex,Pattern.CASE_INSENSITIVE);
 		Matcher matcher = emailPat.matcher(input);
 //		return "matcher.find(): "+matcher.find();
